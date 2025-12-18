c4a249f7e9574eb2fe2316affcde2bdd91ada349
arunsla <arunraj.softlogic@gmail.com>
2025-12-18 18:31:05 +0530

    update

---
+++ JavaScript/day 2/d2.js
@@ -0,0 +1,110 @@
+// variable
+// var let  const
+
+// var a = 12; // dclaring
+
+// console.log(a);
+
+// var a = 45;
+
+// console.log(a);
+
+// a = 56; // resignng
+
+// console.log(a);
+
+// console.log(b);
+
+// var b = 90;
+
+// console.log(b);
+
+// var c = 40;
+
+// {
+//     var c = 90;
+// }
+
+// var c = 89;
+
+// c = 77;
+
+// console.log(c);
+
+// let , const 
+
+
+// let ab = 90;
+// console.log(ab);
+
+// let rr ;
+
+
+// rr = 990;
+
+// let bb = 55;
+
+
+// {
+//     let cc = 77
+//     console.log(cc);
+    
+// }
+
+// console.log(cc);
+
+
+// string 
+
+
+// let name = "Arun"
+
+// console.log(name)
+
+// let age = 35;
+// console.log(typeof age);
+
+// let price = 99.88;
+
+
+
+// boolian 
+
+// let oisLoggedIn = true ;
+
+
+// console.log( oisLoggedIn);
+
+
+// let city ;
+// city = "chennai"
+
+
+// console.log(city);
+
+
+
+let user = {
+    name : "arun",
+    age : 25,
+
+}
+
+
+console.log( typeof user);
+
+let ar = null ;
+
+console.log( ar);
+
+
+
+
+
+
+
+
+
+
+
+
