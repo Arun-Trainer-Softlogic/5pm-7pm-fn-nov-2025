d2eeaa317f75651b37d3c0b45a707535053e309a
arunsla <arunraj.softlogic@gmail.com>
2025-12-31 18:51:42 +0530

    day 6

---
+++ JavaScript/Day 6/script.js
@@ -0,0 +1,98 @@
+// function greet(){
+//     console.log("Hello welcome");
+
+// }
+
+// greet();
+
+// function greetuser(name = "guest"){
+//     console.log("Hello "+ name);
+// }
+
+// greetuser("raj")
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// let result = add(10, 56);
+
+// let bb = result + 57;
+
+// console.log(bb);
+
+function add(a, b) {
+  return a + b;
+}
+
+// function calculate() {
+//   let a = Number(document.getElementById("num1").value);
+ 
+  
+//   let b = Number(document.getElementById("num2").value);
+
+//   let sum = add(a, b);
+
+//   document.getElementById("result").innerText = "Result :" + sum;
+// }
+
+
+
+// function add(a,b){
+//   return a + b
+// }
+
+
+// const add = (a,b) => {
+//   return a + b 
+// }
+
+// console.log(add(34, 78));
+
+// const greet = name => "hello" + name ;
+
+
+// console.log(greet("arun"));
+
+
+
+// const sayHi = ()=> {
+//   console.log("hello there ");
+// }
+
+
+// sayHi();
+
+
+
+
+// const numbers = [1,2,3,4,5]
+
+// const squares = numbers.map(n => n%n)
+
+// console.log(squares);
+
+
+
+// function greet (name , callback){
+//   console.log("hello "+ name);
+//   callback()
+// }
+
+
+// function sayBye (){
+//   console.log("Goodbye ");
+  
+// }
+
+
+// greet("Arun", sayBye)
+
+
+
+(function (){
+    alert("IIfe is good");
+    
+})()
+
+
