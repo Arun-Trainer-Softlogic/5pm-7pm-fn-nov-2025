0468915bdf6a7057bc6d7e96fcc2c1cb1a776dfa
arunsla <arunraj.softlogic@gmail.com>
2025-12-31 10:53:34 +0530

    update

---
+++ JavaScript/Day 5/script.js
@@ -0,0 +1,87 @@
+// while (condition ){
+//     // code 
+// }
+
+
+// let i = 1 ;
+
+
+
+// while (i <= 5 ) {
+//     console.log(i);
+//     i++
+// }
+
+
+
+
+// for (initilize ; condition ; increment ){
+//     // coide odum 
+// }
+
+
+// for (let i = 1 ; i <= 5 ; i++ ){
+//     console.log(i);
+    
+// }
+
+
+
+// do {
+//     // code 
+// }while (conditipo)
+
+
+
+
+// let i = 3;
+// do{
+//     console.log(i);
+//     i++;
+
+   
+// }while (i <= 5)
+
+
+
+// let user = {
+//     name : "ar",
+//     age : 20,
+//     place : "canada"
+// }
+
+// // for (key  in object ){
+// //     // code 
+// // }
+
+// for ( let  key  in user ){
+//     console.log(key + ": " + user[key]);
+    
+// }
+
+
+// for .. of 
+
+// const fruits = "strawberry"
+
+
+// for ( let fru of fruits ){
+//     console.log(fru);
+    
+// }
+
+// console.log(fruits);
+// function  ar(){
+//     console.log("hello ar ");
+// }
+
+// ar()
+
+
+// function greetUser(name = "Guest" ){
+//     console.log("hello "+ name);
+    
+// }
+
+
+// greetUser("arun")
