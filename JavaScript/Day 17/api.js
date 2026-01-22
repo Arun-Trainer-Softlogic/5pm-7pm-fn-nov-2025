517394ae7e2964c811121263c54c0b6d3a11295d
arunsla <arunraj.softlogic@gmail.com>
2026-01-22 18:27:51 +0530

    jj

---
+++ JavaScript/Day 17/api.js
@@ -0,0 +1,37 @@
+// fetch(url)
+// .then (response => response.json())
+// .then (data => {
+//     console.log(data);
+    
+// })
+
+// .catch (err => {
+//     console.error('Error fetching data:', err);
+// })
+
+// fetch ("https://api.github.cm/users/arun-cloud-dev/followers")
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data);
+// } )
+// .catch(error => {
+//     console.error("Error Fetching Followers : " , error)
+// })
+
+async function getFollowers() {
+    try{
+        const response = await fetch("https://api.github.com/users/arun-cloud-dev/followers")
+        const followers = await response.json();
+        console.log(followers);
+    }catch(error){
+        console.error(error)
+    }
+}
+
+
+getFollowers();
+
+
+
+
+
