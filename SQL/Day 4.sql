581c1ef40a5c4a3ac6493ca862805bca11a4a3b6
arunsla <arunraj.softlogic@gmail.com>
2026-01-31 11:28:51 +0530

    update

---
+++ SQL/Day 4.sql
@@ -0,0 +1,115 @@
+-- use 5pmclass;
+
+-- select * from employees ;
+
+-- select emp_name , salary 
+-- from employees 
+-- where salary > 45000
+
+-- select emp_name , salary from employees
+-- where dept_id =2  and salary > 50000 ;
+
+
+-- select distinct dept_id
+-- from employees ;
+
+-- select emp_name , salary 
+-- from employees 
+-- order by salary ;
+
+-- select emp_name, min(salary )
+-- from employees ;
+
+-- INSERT INTO employees (dept_id)
+-- VALUES
+-- (2) where emp_id = 24
+
+-- update employees set dept_id = 2 where emp_id = 24
+
+-- select dept_id, avg(salary)
+-- from employees 
+-- group by dept_id;
+
+-- select dept_id , avg(salary)
+-- from employees 
+-- group by dept_id 
+-- having avg(salary) > 50000 ; 
+
+
+-- select emp_name , status
+-- from employees 
+-- where status = 'active'
+
+
+-- select emp_name , salary 
+-- from employees 
+-- order by salary desc 
+-- limit 3;
+
+
+
+-- select dept_id , count(*)
+-- from employees
+-- group by dept_id
+
+
+-- select dept_id , avg(salary)
+-- from employees 
+-- group by dept_id
+-- having avg(salary) > 50000  
+
+-- What does DISTINCT do?
+-- Can ORDER BY work without WHERE?
+
+
+-- select emp_name , salary 
+-- from employees 
+-- where dept_id = 1
+-- order by salary desc;
+
+-- Why GROUP BY is needed?
+
+-- select dept_id , avg(salary)
+-- from employees 
+-- group by dept_id ;
+
+-- Difference between WHERE and HAVING?
+
+-- select emp_name , salary 
+-- from employees 
+-- where salary > 50000;
+
+
+
+
+-- select dept_id , avg(salary)
+-- from employees 
+-- where status = "active"
+-- group by dept_id 
+-- having count(*) > 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
